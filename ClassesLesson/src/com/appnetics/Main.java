d351cc54e51230fccb3bca9f99fa126d8c6dfefe
Odero Oluoch <52481094+0deroOluoch@users.noreply.github.com>
2020-04-22 04:32:57 +0300

    Add files via upload

---
+++ ClassesLesson/src/com/appnetics/Main.java
@@ -0,0 +1,10 @@
+package com.appnetics;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Car porsche = new Car();
+	porsche.setModel("Carrera");
+        System.out.println("The model is " + porsche.getModel());
+    }
+}
